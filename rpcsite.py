1c624e2b62b9d3f5c2df454fe85d09de2448c34a
maselkov <Maselkov@users.noreply.github.com>
2017-12-18 19:42:53 +0100

    Add more images, fix wrong information

--- rpcsite.py
+++ rpcsite.py
@@ -5,7 +5,7 @@ from flask import Flask, abort, jsonify, render_template, send_file
 
 app = Flask(__name__)
 
-CLIENT_VERSION = 0.4
+CLIENT_VERSION = 1.0
 
 
 RANDOM_IMAGE_POOL = os.listdir("static/img/showcases/")
