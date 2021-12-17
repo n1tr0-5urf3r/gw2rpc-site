5ab3827be926e52e365eab18eac40536db54e63e
Fabian Ihle <fabi@ihlecloud.de>
2021-12-17 10:47:01 +0100

    Version bump

--- rpcsite.py
+++ rpcsite.py
@@ -5,7 +5,7 @@ from flask import Flask, abort, jsonify, render_template, send_file
 
 app = Flask(__name__)
 
-CLIENT_VERSION = 2.11
+CLIENT_VERSION = 2.2
 
 RANDOM_IMAGE_POOL = os.listdir("static/img/showcases/")
 
